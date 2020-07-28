5551009584bcbd5afb0e4c39d1c0a10b2d73638c
David Dennis <david.dc.dennis@gmail.com>
2020-07-27 19:00:57 -0500

    added notes to main.js, added button to HTML

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
 'use strict';
 
 console.log("All hail the listener! Praise Sithis!");
+
+// const sayHi = (str) => {
+//     return `Hello, ${str}`;
+// }
+
+// const sayBye = (str) => {
+//     return `Later, ${str}`;
+// }
+
+// const comeOrGo = (str, func) => {
+//     return func(str);
+// }
+
+// console.log(comeOrGo('Bailey', sayHi));
+// console.log(comeOrGo('Bailey', sayBye));
+
+
+// //by ID only
+// const button = document.getElementById('main-btn');
+// console.log(button);
+
+//by class OR id...
+// const div = document.querySelector('#divId'); OR . for classes
+// for query selectior, it will ONLY select the first one in a given class!
+
+// const otherBtn = document.querySelector('#main-btn');
+// console.log(otherBtn);
+
+/* div.addeventListner (click, () => {
+    console.log('mouse has entered!');
+} */
+
+// const logIt = () => {
+//     console.log("You clicked me!");
+// }
+
+// const button = document.getElementById('main-btn'); //or you can use querySelector!
+
+// button.addEventListener('click', logIt);
+
